0106073c55dc70041809aea3462f495ee891deb8
Jens Pots <jens@jenspots.com>
2024-05-28 17:00:40 +0200

    feat: add github package dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,32 @@
-plugins { id("java") }
+plugins {
+  id("java")
+  id("co.uzzu.dotenv.gradle") version "4.0.0"
+}
 
+/** Package data. */
 group = "technology.idlab"
+version = "0.0.1"
 
-version = "1.0-SNAPSHOT"
+/** Use JUnit for testing. */
+tasks.test { useJUnitPlatform() }
 
-repositories { mavenCentral() }
+repositories {
+  mavenCentral()
+
+  maven {
+    url = uri("https://maven.pkg.github.com/rdf-connect/jvm-runner")
+    credentials {
+      username = env.GITHUB_ACTOR.value
+      password = env.GITHUB_TOKEN.value
+    }
+  }
+}
 
 dependencies {
+  // JVM Runner
+  implementation("technology.idlab:jvm-runner:0.0.1")
+
+  // JUnit
   testImplementation(platform("org.junit:junit-bom:5.10.0"))
   testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
-tasks.test { useJUnitPlatform() }
